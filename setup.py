1ddcec1b98b712179a9a53cdd1056e705af161b9
Captain Crypto <tamir@crypto-knight.tech>
2024-01-29 21:57:07 +0200

    requirements.txt update

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpell',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'uuid',
-        'threading',
         'asyncio',
         'websocket-client',
         # ... other dependencies
